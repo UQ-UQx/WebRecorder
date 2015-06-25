bdc1e2e42184db39c825a83ccca1c4ae7f257967
simultech <simultech@gmail.com>
2015-06-25 13:58:21 +1000

    Initial Commit

---
+++ WebRecorder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebRecorder
+//
+//  Created by uqadekke on 25/06/2015.
+//  Copyright (c) 2015 uqadekke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebRecorder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebRecorder
+//
+//  Created by uqadekke on 25/06/2015.
+//  Copyright (c) 2015 uqadekke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
